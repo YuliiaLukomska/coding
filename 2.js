5c53eee6c3dd49a92a0aca198c90977363909428
YuliiaLukomska <142302232+YuliiaLukomska@users.noreply.github.com>
2023-11-01 22:39:06 +0200

    add 2 js file

---
+++ 2.js
@@ -0,0 +1,55 @@
+'use strict';
+
+// function calculateEvenTotal(number) {
+//   let total = 0;
+//   for (let index = 0; number >= index; index += 1) {
+//     if (index % 2 === 0) {
+//       total = total + index;
+//     } else {
+//       total = total;
+//     }
+//   }
+//   return total;
+// }
+
+// const result = calculateEvenTotal(0);
+// console.log(result);
+
+// const start = 6;
+// const end = 17;
+// let number;
+
+// function getNumber(start, end) {
+//   for (let i = start; i <= end; i += 1) {
+//     if (i % 5 === 0) {
+//       number = i;
+//       break;
+//     }
+//   }
+//   return number;
+// }
+// const result = getNumber(6, 17);
+// console.log(result);
+
+// const start = 17;
+// const end = 25;
+// let number;
+// for (let i = start; i <= end; i += 1) {
+//   if (i % 5 === 0) {
+//     number = i;
+//     break;
+//   }
+// }
+// console.log(number);
+
+// let a;
+// for (let i = 6; i > 3; i--) {
+//   a = i;
+// }
+// console.log(a);
+
+let i;
+for (let i = 6; i > 3; i--) {
+  i = i;
+}
+console.log(i);
